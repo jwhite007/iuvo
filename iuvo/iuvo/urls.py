06fa702ef68181c404edb3515b7e56afac3f5ce6
James White <jwhite007@comcast.net>
2014-10-30 09:41:27 -0700

    deleted registration urls and added allauth urls in iuvo/urls.py

--- iuvo/iuvo/urls.py
+++ iuvo/iuvo/urls.py
@@ -1,23 +1,14 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 # from iuvo_app.forms import RegisterForm
 # import iuvo_app.urls
+# from django.conf.urls.static import static
 admin.autodiscover()
 
-urlpatterns = patterns(
-    '',
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^', include('iuvo_app.urls')),
-    url(r'^accounts/',
-        include('registration.backends.default.urls')),
-    # url(r'^accounts/$',
-    #     register,
-    #     {'backend': 'registration.backends.default.DefaultBackend',
-    #     'form_class': RegisterForm})
-)
-
-
-# (r'^register/$',
-#     register,
-#     {'backend': 'registration.backends.default.DefaultBackend',
-#      'form_class': MyExtendedForm}),
+urlpatterns = patterns('',
+                       url(r'^admin/', include(admin.site.urls)),
+                       url(r'^', include('iuvo_app.urls')),
+                       url(r'^accounts/',
+                           include('allauth.urls')),
+                       )
